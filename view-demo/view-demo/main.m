5b77de36a305372f40902c21f03bb214e84f80b1
John Bender <john@generalui.com>
2013-01-25 11:13:30 -0700

    view demo project to demonstrate simple drawing with UIBezierPath

---
+++ view-demo/view-demo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  view-demo
+//
+//  Created by John Bender on 1/25/13.
+//  Copyright (c) 2013 General UI, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "VHAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([VHAppDelegate class]));
+    }
+}
---
+++ view-demo/view-demoTests/view_demoTests.m
@@ -0,0 +1,32 @@
+//
+//  view_demoTests.m
+//  view-demoTests
+//
+//  Created by John Bender on 1/25/13.
+//  Copyright (c) 2013 General UI, LLC. All rights reserved.
+//
+
+#import "view_demoTests.h"
+
+@implementation view_demoTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in view-demoTests");
+}
+
+@end
