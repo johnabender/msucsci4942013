98289bcee1a1860d772948a0541953e30db3923d
Andrew <andrew.w.wilson.aww@gmail.com>
2013-01-29 13:02:02 -0700

    assignment 1
    First files

---
+++ Assignment 1/iOS Assignment 1/iOS Assignment 1Tests/iOS_Assignment_1Tests.h
@@ -0,0 +1,13 @@
+//
+//  iOS_Assignment_1Tests.h
+//  iOS Assignment 1Tests
+//
+//  Created by Andrew Wilson on 1/28/13.
+//  Copyright (c) 2013 Andrew Wilson. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface iOS_Assignment_1Tests : SenTestCase
+
+@end
